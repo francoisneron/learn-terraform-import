8de2ef0134eca6886d8dbeeb8deaa7971720559b
francoisneron <fneron@hashicorp.com>
2020-10-23 14:28:38 -0400

    Link existing docke image to TF config

--- docker.tf
+++ docker.tf
@@ -7,13 +7,12 @@
 # }
 #
 # # docker_container.web:
-# resource "docker_container" "web" {
-#   name  = "hashicorp-learn"
-#
-#   image = docker_image.nginx.latest
-#
-#   ports {
-#     external = 8081
-#     internal = 80
-#   }
-# }
+resource "docker_container" "web" {
+  name              = "hashicorp-learn"
+  image             = "sha256:f35646e83998b844c3f067e5a2cff84cdf0967627031aeda3042d78996b68d35"
+
+  ports {
+    external = 8080
+    internal = 80
+  }
+}
